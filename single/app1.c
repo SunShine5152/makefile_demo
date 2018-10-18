b50ec4c332bef8a8e3156350d659955e3ad311e5
liyingjie <yingjie79613love@126.com>
2018-10-18 16:02:11 +0800

    2 finish single

---
+++ single/app1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+	printf("hello,this is app1\n");
+
+	return 0;
+}
+
---
+++ single/app3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello,this is app3\n");	
+
+
+	return 0;
+}
+
